5c059d3ea651d5d7110e54c95f64984c620635f1
Ouz <ouzdeville@gmail.com>
2022-03-29 16:18:13 +0000

    ds

---
+++ src/controller/user.ts
@@ -0,0 +1,12 @@
+import Book from "../entity/book";
+import { Request, Response } from 'express';
+
+
+export default class UsserController {
+
+    static create(req: Request, res: Response) {
+      
+    }
+
+    
+}
