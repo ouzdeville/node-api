e814164cf4e2694a3b2ab28e18dc724bdfe45c7f
Assane Ali Boye <aalijr97@gmail.com>
2022-11-07 18:52:57 +0000

    sd,qs,;

--- src/entity/filiere.ts
+++ src/entity/filiere.ts
@@ -6,6 +6,7 @@ let filiereSchema= new mongoose.Schema({
     nom:{type: String, required: true },
     sigle:{type: String, required: true },
     niveaux: [{ type: Schema.Types.ObjectId, ref: 'niveau' }],
+    departement: { type: Schema.Types.ObjectId, ref: 'departement' },
 });
 
 const Filiere=mongoose.model("filiere", filiereSchema);
--- src/entity/niveau.ts
+++ src/entity/niveau.ts
@@ -6,6 +6,7 @@ let niveauSchema= new mongoose.Schema({
     nom:{type: String, required: true },
     sigle:{type: String, required: true },
     promos: [{ type: Schema.Types.ObjectId, ref: 'promo' }],
+    filiere: {type: Schema.Types.ObjectId, ref:"filiere"}
 });
 
 const Niveau=mongoose.model("niveau", niveauSchema);
